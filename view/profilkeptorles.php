21b6a9554237d447c2ccd7d564084d50401fba1c
FazekasMiklos <miklos81006@gmail.com>
2022-02-24 20:25:49 +0100

    profilkép törlés

---
+++ view/profilkeptorles.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="HU">
+<head>
+<link rel = "stylesheet" type = "text/css" href = "style.css">   
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<div id="btn"> 
+<div id="reg">
+ 	<form action='index.php?page=profilepicdelete' method='POST'>
+		<label for="user">Biztos hogy törölni akarod a profilképed?</label>	<br/>
+		<input type='submit' class="btn mb-2" style="background-color:#228B22; color:white;" name='igen' id="igen" value = "Igen" />
+        <input type='submit' class="btn mb-2" style="background-color:#228B22; color:white;" name='nem' id="nem" value = "Nem" />
+	</form>
+</div>
+</div>
+</body>
+</html>
