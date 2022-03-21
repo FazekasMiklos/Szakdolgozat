75199716cb55edbe4c1c401aab23a599aab51e66
FazekasMiklos <miklos81006@gmail.com>
2022-03-21 09:23:03 +0100

    ikon

--- includes/htmlheader.inc.php
+++ includes/htmlheader.inc.php
@@ -5,7 +5,8 @@
 <html lang="hu">
 <head>
 	<meta charset="utf-8">
-	<title><?php echo $title; ?></title>	
+	<title><?php echo $title; ?></title>
+    <link rel="icon" type="image/x-icon" href="kepek/labda.png">	
     <link rel="stylesheet" type="text/css" href="view/style.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel='stylesheet' href='https://raw.githubusercontent.com/kartik-v/bootstrap-star-rating/master/css/star-rating.min.css'>
